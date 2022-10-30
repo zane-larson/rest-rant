ddbc90380aa20f1262d93bd4f9340381151d90cd
zane-larson <zanelarson2002@gmail.com>
2022-10-30 13:01:49 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello world!')
+})
+
+
+
+app.get('*', (req, res) => {
+    res.send('Hello world!')
+})
+
+app.listen(process.env.PORT)
