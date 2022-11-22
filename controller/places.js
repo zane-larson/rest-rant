1502b803688ad4adcaa2babce6d5397493c52441
zane-larson <zanelarson2002@gmail.com>
2022-11-21 16:26:52 -0800

    part 5

--- controller/places.js
+++ controller/places.js
@@ -1,5 +1,12 @@
 const router = require('express').Router()
 
+
+router.get('/new', (req, res) => {
+
+  res.render('places/new')
+})
+
+
 router.get('/', (req, res) => {
     let places = [{
         name: 'H-Thai-ML',
@@ -17,5 +24,6 @@ router.get('/', (req, res) => {
       
     res.render('places/index', {places})
 })
+
 
 module.exports = router
